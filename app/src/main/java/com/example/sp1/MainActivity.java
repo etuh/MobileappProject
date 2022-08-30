6e365f2aefa9446702af32aae7b30f7d9538ff5b
j3rryl <jeremymunroe0@gmail.com>
2022-08-30 19:20:11 +0300

    Yaay

---
+++ app/src/main/java/com/example/sp1/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.sp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+public class MainActivity extends AppCompatActivity {
+    MeowBottomNavigation bottomNavigation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation=findViewById(R.id.btm_nav);
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_baseline_person_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_baseline_format_list_bulleted_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.ic_baseline_attach_money_24));
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment fragment=null;
+                switch (item.getId()){
+                    case 1:
+                        fragment=new ProfileFragment();
+                        break;
+                    case 2:
+                        fragment=new JobsFragment();
+                        break;
+                    case 3:
+                        fragment=new WalletFragment();
+                        break;
+                }
+                loadFragment(fragment);
+            }
+        });
+        bottomNavigation.setCount(2,"3");
+        bottomNavigation.show(2,true);
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext(),"You selected "+item.getId(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext(),"Reselected",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout,fragment)
+                .commit();
+    }
+
+}
